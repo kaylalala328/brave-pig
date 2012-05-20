676aae77ded110b5451a52afaa918260fdcb1715
sonan0721@gmail.com <sonan0721@gmail.com@51f6563e-01e0-5241-4797-f52fb3fef207>
2012-05-20 12:45:11 +0000

    [도행]GameObject 및 AnimationStrip 설정

---
+++ Brave-Pig/Brave-Pig/Brave-Pig/AnimationStrip.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Gemstone_Hunter
+{
+    public class AnimationStrip
+    {
+        #region Declarations
+        //애니메이션 텍스쳐
+        private Texture2D texture;
+
+        //애니메이션 frame width
+        private int frameWidth;
+        //애니메이션 frame height
+        private int frameHeight;
+
+        //frame Timer
+        private float frameTimer = 0f;
+        //frame delay
+        private float frameDelay = 0.05f;
+
+        //현재 애니메이션 frme
+        private int currentFrame;
+
+        //애니메이션을 반복할 것인가
+        private bool loopAnimation = true;
+        //애니메이션이 끝났는가?
+        private bool finishedPlaying = false;
+
+        //애니메이션 이름
+        private string name;
+        //다음 애니메이션 이름
+        private string nextAnimation;
+        #endregion
+
+        #region Properties
+        public int FrameWidth
+        {
+            get { return frameWidth; }
+            set { frameWidth = value; }
+        }
+
+        public int FrameHeight
+        {
+            get { return frameHeight; }
+            set { frameHeight = value; }
+        }
+
+        public Texture2D Texture
+        {
+            get { return texture; }
+            set { texture = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string NextAnimation
+        {
+            get { return nextAnimation; }
+            set { nextAnimation = value; }
+        }
+
+        public bool LoopAnimation
+        {
+            get { return loopAnimation; }
+            set { loopAnimation = value; }
+        }
+
+        public bool FinishedPlaying
+        {
+            get { return finishedPlaying; }
+        }
+
+        public int FrameCount
+        {
+            get { return texture.Width / frameWidth; }
+        }
+
+        public float FrameLength
+        {
+            get { return frameDelay; }
+            set { frameDelay = value; }
+        }
+
+        public Rectangle FrameRectangle
+        {
+            get
+            {
+                return new Rectangle(
+                    currentFrame * frameWidth,
+                    0,
+                    frameWidth,
+                    frameHeight);
+            }
+        }
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Animation Stip 생성자
+        /// </summary>
+        /// <param name="texture">애니메이션 Texture 파일(Spritesheet)</param>
+        /// <param name="frameWidth">Frame의 총 Wdith</param>
+        /// <param name="name">애니메이션 이름</param>
+        public AnimationStrip(Texture2D texture, int frameWidth, string name)
+        {
+            this.texture = texture;
+            this.frameWidth = frameWidth;
+            this.frameHeight = texture.Height;
+            this.name = name;
+        }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// 애니메이션의 시작을 알려줌 FinishPlaying을 false로 바꿈
+        /// </summary>
+        public void Play()
+        {
+            currentFrame = 0;
+            finishedPlaying = false;
+        }
+
+        /// <summary>
+        /// 애니메이션 업데이트 함수
+        /// </summary>
+        /// <param name="gameTime">현재 게임 시간</param>
+        public void Update(GameTime gameTime)
+        {
+            //elapsed time
+            float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            //Timer에 elapsed를 더함
+            frameTimer += elapsed;
+
+            //FrameTimer >= Delay 일정 시간이 지나면, 
+            if (frameTimer >= frameDelay)
+            {
+                //현재 프레임을 더함
+                currentFrame++;
+                //Framecount
+                if (currentFrame >= FrameCount)
+                {
+                    //반복
+                    if (loopAnimation)
+                    {
+                        currentFrame = 0;
+                    }
+                    //반복 아니라면
+                    else
+                    {
+                        currentFrame = FrameCount - 1;
+                        finishedPlaying = true;
+                    }
+                }
+
+                frameTimer = 0f;
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ Brave-Pig/Brave-Pig/Brave-Pig/GameObject.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Tile_Engine;
+
+namespace Gemstone_Hunter
+{
+    public class GameObject
+    {
+        #region Declarations
+        //세상 위치
+        protected Vector2 worldLocation;
+        //Obejct 속도
+        protected Vector2 velocity;
+        //Frame의 Width
+        protected int frameWidth;
+        //각 Frame의 Height
+        protected int frameHeight;
+
+        //?? 어떤 bool 변수?
+        protected bool enabled;
+        //flipped?? 어떤 flipped?
+        protected bool flipped = false;
+
+        //on ground? 땅에 있을때?
+        protected bool onGround;
+
+        //충돌 사각형
+        protected Rectangle collisionRectangle;
+        //충돌 width
+        protected int collideWidth;
+        //충동 height
+        protected int collideHeight;
+        //code base? block??
+        protected bool codeBasedBlocks = true;
+
+        //그림 depth?? 위치 위에서 아래로
+        protected float drawDepth = 0.85f;
+        
+        //Animation Dictionary
+        protected Dictionary<string, AnimationStrip> animations =
+            new Dictionary<string, AnimationStrip>();
+        
+        //현재 애니메이션 string
+        protected string currentAnimation;
+        #endregion
+
+        #region Properties
+
+        public bool Enabled
+        {
+            get { return enabled; }
+            set { enabled = value; }
+        }
+
+        public Vector2 WorldLocation
+        {
+            get { return worldLocation; }
+            set { worldLocation = value; }
+        }
+
+        public Vector2 WorldCenter
+        {
+            get
+            {
+                return new Vector2(
+                  (int)worldLocation.X + (int)(frameWidth / 2),
+                  (int)worldLocation.Y + (int)(frameHeight / 2));
+            }
+        }
+
+        public Rectangle WorldRectangle
+        {
+            get
+            {
+                return new Rectangle(
+                    (int)worldLocation.X,
+                    (int)worldLocation.Y,
+                    frameWidth,
+                    frameHeight);
+            }
+        }
+
+        public Rectangle CollisionRectangle
+        {
+            //사가곃ㅇ 위치
+            get
+            {
+                return new Rectangle(
+                    (int)worldLocation.X + collisionRectangle.X,
+                    (int)WorldRectangle.Y + collisionRectangle.Y,
+                    collisionRectangle.Width,
+                    collisionRectangle.Height);
+            }
+            set { collisionRectangle = value; }
+        }
+        #endregion 
+
+        #region Helper Methods
+        
+        //애니메이션 업데이트
+        private void updateAnimation(GameTime gameTime)
+        {
+            //Key를 갖고 잇어야함. 즉 애니메이션이 존재하면,
+            if (animations.ContainsKey(currentAnimation))
+            {
+                //애니메이션이 끝났다면
+                if (animations[currentAnimation].FinishedPlaying)
+                {
+                    //애니메이션은 다음 애니메이션을 가르키게 한다.
+                    PlayAnimation(animations[currentAnimation].NextAnimation);
+                }
+                else
+                {
+                    //애니메이션이 안끝낫다면
+                    animations[currentAnimation].Update(gameTime);
+                }
+            }
+        }
+        #endregion
+
+        #region Map-Based Collision Detection Methods
+        private Vector2 horizontalCollisionTest(Vector2 moveAmount)
+        {
+            if (moveAmount.X == 0)
+                return moveAmount;
+
+            Rectangle afterMoveRect = CollisionRectangle;
+            afterMoveRect.Offset((int)moveAmount.X, 0);
+            Vector2 corner1, corner2;
+
+            if (moveAmount.X < 0)
+            {
+                corner1 = new Vector2(afterMoveRect.Left,
+                                      afterMoveRect.Top + 1);
+                corner2 = new Vector2(afterMoveRect.Left,
+                                      afterMoveRect.Bottom - 1);
+            }
+            else
+            {
+                corner1 = new Vector2(afterMoveRect.Right,
+                                      afterMoveRect.Top + 1);
+                corner2 = new Vector2(afterMoveRect.Right,
+                                      afterMoveRect.Bottom - 1);
+            }
+
+            Vector2 mapCell1 = TileMap.GetCellByPixel(corner1);
+            Vector2 mapCell2 = TileMap.GetCellByPixel(corner2);
+
+            if (!TileMap.CellIsPassable(mapCell1) ||
+                !TileMap.CellIsPassable(mapCell2))
+            {
+                moveAmount.X = 0;
+                velocity.X = 0;
+            }
+
+            if (codeBasedBlocks)
+            {
+                if (TileMap.CellCodeValue(mapCell1) == "BLOCK" ||
+                    TileMap.CellCodeValue(mapCell2) == "BLOCK")
+                {
+                    moveAmount.X = 0;
+                    velocity.X = 0;
+                }
+            }
+
+            return moveAmount;
+        }
+
+        private Vector2 verticalCollisionTest(Vector2 moveAmount)
+        {
+            if (moveAmount.Y == 0)
+                return moveAmount;
+
+            Rectangle afterMoveRect = CollisionRectangle;
+            afterMoveRect.Offset((int)moveAmount.X, (int)moveAmount.Y);
+            Vector2 corner1, corner2;
+
+            if (moveAmount.Y < 0)
+            {
+                corner1 = new Vector2(afterMoveRect.Left + 1,
+                                      afterMoveRect.Top);
+                corner2 = new Vector2(afterMoveRect.Right - 1,
+                                      afterMoveRect.Top);
+            }
+            else
+            {
+                corner1 = new Vector2(afterMoveRect.Left + 1,
+                                      afterMoveRect.Bottom);
+                corner2 = new Vector2(afterMoveRect.Right - 1,
+                                      afterMoveRect.Bottom);
+            }
+
+            Vector2 mapCell1 = TileMap.GetCellByPixel(corner1);
+            Vector2 mapCell2 = TileMap.GetCellByPixel(corner2);
+
+            if (!TileMap.CellIsPassable(mapCell1) ||
+                !TileMap.CellIsPassable(mapCell2))
+            {
+                if (moveAmount.Y > 0)
+                    onGround = true;
+                moveAmount.Y = 0;
+                velocity.Y = 0;
+            }
+
+            if (codeBasedBlocks)
+            {
+                if (TileMap.CellCodeValue(mapCell1) == "BLOCK" ||
+                    TileMap.CellCodeValue(mapCell2) == "BLOCK")
+                {
+                    if (moveAmount.Y > 0)
+                        onGround = true;
+                    moveAmount.Y = 0;
+                    velocity.Y = 0;
+                }
+            }
+
+            return moveAmount;
+        }
+
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Animation 입력이 들어오면 애니메이션을 Play
+        /// </summary>
+        /// <param name="name"></param>
+        public void PlayAnimation(string name)
+        {
+            if (!(name == null) && animations.ContainsKey(name))
+            {
+                currentAnimation = name;
+                animations[name].Play();
+            }
+        }
+
+        /// <summary>
+        /// 업데이트 함수
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public virtual void Update(GameTime gameTime)
+        {
+            //가능하다면,
+            if (!enabled)
+                return;
+
+            //현재 elapsed time
+            float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            //애니메이션을 업데이트함.
+            updateAnimation(gameTime);
+
+            //속도의 Y가 0이 아니라면, ground false 즉 점프 될 수 있음.
+            if (velocity.Y != 0)
+            {
+                onGround = false;
+            }
+
+            //이동량을 속도 * elapsed
+            Vector2 moveAmount = velocity * elapsed;
+
+            //충돌 관련함수
+            moveAmount = horizontalCollisionTest(moveAmount);
+            moveAmount = verticalCollisionTest(moveAmount);
+
+            //moveAmount를 더한 새로운 포지션을 가짐
+            Vector2 newPosition = worldLocation + moveAmount;
+
+            //*****check 무엇을 clamp하는지
+            //새로운 포지션(clamp 함수 이용)
+            newPosition = new Vector2(
+                MathHelper.Clamp(newPosition.X, 0,
+                  Camera.WorldRectangle.Width - frameWidth),
+                MathHelper.Clamp(newPosition.Y, 2 * (-TileMap.TileHeight),
+                  Camera.WorldRectangle.Height - frameHeight));
+
+            //newposition을 worldlocation에 저장함.
+            worldLocation = newPosition;
+        }
+
+        /// <summary>
+        /// 그리기 함수
+        /// </summary>
+        /// <param name="spriteBatch"></param>
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            //가능하지 않다면 return
+            if (!enabled)
+                return;
+
+            //애니메이션을 갖고 잇으면
+            if (animations.ContainsKey(currentAnimation))
+            {
+                //spriteeffect 좌우 대칭을 위한 Effect 변수
+                SpriteEffects effect = SpriteEffects.None;
+
+                //flip 시킬 것인가?
+                if (flipped)
+                {
+                    effect = SpriteEffects.FlipHorizontally;
+                }
+
+                //현재 spriteBatch를 그린다.
+                spriteBatch.Draw(
+                    animations[currentAnimation].Texture,
+                    Camera.WorldToScreen(WorldRectangle),
+                    animations[currentAnimation].FrameRectangle,
+                    Color.White, 0.0f, Vector2.Zero, effect, drawDepth);
+            }
+        }
+
+        #endregion
+
+    }
+}
