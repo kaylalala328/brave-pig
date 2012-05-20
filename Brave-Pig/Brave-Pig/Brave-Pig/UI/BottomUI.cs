552df9795c404bec06cb3e9e38d00244dd4861bf
sonan0721@gmail.com <sonan0721@gmail.com@51f6563e-01e0-5241-4797-f52fb3fef207>
2012-05-20 17:39:55 +0000

    [정기] weapon UI & status UI 추가

--- Brave-Pig/Brave-Pig/Brave-Pig/UI/BottomUI.cs
+++ Brave-Pig/Brave-Pig/Brave-Pig/UI/BottomUI.cs
@@ -14,26 +14,60 @@ namespace Brave_Pig.UI
     class BottomUI
     {
         Texture2D bottomUI;
+        Texture2D weapon;
         int width, height;
+
+        bool changeWeapon;
+        float rotate, rotate1, rotate2, rotate3;
 
         public void Initialize(GraphicsDevice graphics)
         {
             width = graphics.Viewport.Width;
             height = graphics.Viewport.Height;
+
+            changeWeapon = false;
+            rotate = 0.0f;
         }
         public void LoadContent(ContentManager content)
         {
             bottomUI = content.Load<Texture2D>("UI/BottomUI");
+            weapon = content.Load<Texture2D>("UI/weapon");
         }
         public void Update(GameTime gameTime)
         {
-
+            if (Game1.previousKeyState.IsKeyDown(Keys.A) && Game1.currentKeyState.IsKeyUp(Keys.A))
+            {
+                changeWeapon = true;
+            }
         }
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(bottomUI,
                 new Rectangle(0, height / 4 * 3, width, height / 4),
                 Color.White);
+            if (changeWeapon)
+            {
+                if (rotate < rotate1 + 2.05f)
+                {
+                    rotate += 0.1f;
+                    spriteBatch.Draw(weapon,
+                    new Rectangle(width / 8 * 7, height / 7 * 6, 80, 80),
+                    null,
+                    Color.White, rotate, new Vector2(80, 80), SpriteEffects.None, 0f);
+                }
+                else
+                {
+                    changeWeapon = false;
+                }
+            }
+            else
+            {
+                spriteBatch.Draw(weapon,
+                    new Rectangle(width / 8 * 7, height / 7 * 6, 80, 80),
+                    null,
+                    Color.White, rotate, new Vector2(80, 80), SpriteEffects.None, 0f);
+                rotate1 = rotate;
+            }
         }
     }
 }
--- Brave-Pig/Brave-Pig/Brave-Pig/UI/status.cs
+++ Brave-Pig/Brave-Pig/Brave-Pig/UI/status.cs
@@ -16,14 +16,10 @@ namespace Brave_Pig.UI
         Texture2D status;
         int width, height;
 
-        bool OpenStatus;
-
         public void Initialize(GraphicsDevice graphics)
         {
             width = graphics.Viewport.Width;
             height = graphics.Viewport.Height;
-
-            OpenStatus = false;
         }
         public void LoadContent(ContentManager content)
         {
@@ -31,22 +27,14 @@ namespace Brave_Pig.UI
         }
         public void Update(GameTime gameTime)
         {
-            if (Game1.previousKeyState.IsKeyDown(Keys.I) && Game1.currentKeyState.IsKeyUp(Keys.I))
-            {
-                if (OpenStatus)
-                    OpenStatus = false;
-                else
-                    OpenStatus = true;
-            }
+            
         }
         public void Draw(SpriteBatch spriteBatch)
         {
-            if (OpenStatus)
-            {
-                spriteBatch.Draw(status,
-                    new Rectangle(width / 8, height / 6, width / 4 * 3, height / 3 * 2),
-                    Color.White);
-            }
+            spriteBatch.Draw(status,
+                new Rectangle(0, 0, width / 6, height / 6),
+                null,
+                Color.White * 0.5f, 0f, Vector2.Zero, SpriteEffects.None, 0f);
         }
     }
 }
