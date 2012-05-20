a772193fa97468ebe0bf12766d58054e96c8bd7a
sonan0721@gmail.com <sonan0721@gmail.com@51f6563e-01e0-5241-4797-f52fb3fef207>
2012-05-20 09:05:31 +0000

    Nus - status + skill tree

--- Brave-Pig/Brave-Pig/Brave-Pig/Character/Status.cs
+++ Brave-Pig/Brave-Pig/Brave-Pig/Character/Status.cs
@@ -2,24 +2,67 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace Brave_Pig.Character
 {
-    class Status
+    public class Status
     {
         public int healPoint;
         public int manaPoint;
         public int damage;
         public int defense;
         public int useSword;
-        
-        /*public void Status()
+
+        public int HealPoint
         {
-            healPoint = 50;
-            manaPoint = 30;
-            damage = 10;
-            defense = 5;
-            useSword = 0;
-        }*/
+            get { return healPoint; }
+            set { healPoint = 50; }
+        }
+
+        public int ManaPoint
+        {
+            get { return manaPoint; }
+            set { healPoint = 30; }
+        }
+
+        public int Damage
+        {
+            get { return damage; }
+            set { damage = 10; }
+        }
+
+        public int Defense
+        {
+            get { return defense; }
+            set { defense = 5; }
+        }
+
+        public int UseSword
+        {
+            get { return useSword; }
+        }
+        public Status()
+        {
+            this.healPoint = 0;
+            this.manaPoint = 0;
+            this.damage = 0;
+            this.defense = 0;
+            this.useSword = 0;
+        }
+        public Status(int heal, int mana, int damage, int defense, int use)
+        {
+            this.healPoint = heal;
+            this.manaPoint = mana;
+            this.damage = damage;
+            this.defense = defense;
+            this.useSword = use;
+        }
     }
 }
--- Brave-Pig/Brave-Pig/Brave-Pig/Character/skill.cs
+++ Brave-Pig/Brave-Pig/Brave-Pig/Character/skill.cs
@@ -2,11 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace Brave_Pig.Character
 {
-    class skill
+    public class skill : Status
     {
-    
+        public void useSkill()
+        {
+            if(UseSword == 0)
+            {
+                //기본 검에 대한 스킬        
+            }
+            else if(UseSword == 1)
+            {
+                //첫번째 검에 대한 스킬
+            }
+            else if(UseSword == 2)
+            {
+                //두번째 검에 대한 스킬
+            }
+            else if(UseSword == 3)
+            {
+                //세번째 검에 대한 스킬
+            }
+        }
     }
 }
