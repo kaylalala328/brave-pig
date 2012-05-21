63dba2b1c2056a616fb9ab80b2c54c313b729ce0
kimjk6463@gmail.com <kimjk6463@gmail.com@51f6563e-01e0-5241-4797-f52fb3fef207>
2012-05-21 02:31:04 +0000

    [정기] status UI 수정

---
+++ Brave-Pig/Brave-Pig/Brave-Pig/UI/WeaponUI.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Brave_Pig.UI
+{
+    class WeaponUI
+    {
+        Texture2D weapon;
+
+        int width, height, count;
+        float rotate, saveRotate;
+        bool changeWeapon, protectChange;
+
+        public void Initialize(GraphicsDevice graphics)
+        {
+            width = graphics.Viewport.Width;
+            height = graphics.Viewport.Height;
+
+            changeWeapon = false;
+            protectChange = false;
+            rotate = 0.0f;
+            saveRotate = 0.0f;
+            count = 0;
+        }
+        public void LoadContent(ContentManager content)
+        {
+            weapon = content.Load<Texture2D>("UI/weapon");
+        }
+        public void Update(GameTime gameTime)
+        {
+            if ( !protectChange )
+            {
+                if ( Game1.previousKeyState.IsKeyDown(Keys.A) && Game1.currentKeyState.IsKeyUp(Keys.A) )
+                {
+                    changeWeapon = true;
+                    protectChange = true;
+                    count++;
+                }
+            }
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if ( changeWeapon )
+            {
+                if ( rotate < saveRotate + 2.05f )
+                {
+                    rotate += 0.1f;
+                    spriteBatch.Draw(weapon,
+                    new Rectangle(width / 8 * 7, height / 7 * 6, 120, 104),
+                    null,
+                    Color.White, rotate, new Vector2(60, 65), SpriteEffects.None, 0f);
+                }
+                else
+                {
+                    changeWeapon = false;
+                    protectChange = false;
+                    if ( count % 3 == 0 )
+                    {
+                        count = 0;
+                        rotate = 0.0f;
+                    }
+                }
+            }
+            else
+            {
+                spriteBatch.Draw(weapon,
+                    new Rectangle(width / 8 * 7, height / 7 * 6, 120, 104),
+                    null,
+                    Color.White, rotate, new Vector2(60, 65), SpriteEffects.None, 0f);
+                saveRotate = rotate;
+            }
+        }
+    }
+}
